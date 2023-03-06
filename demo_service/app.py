c9a1d19bae2e319d3b7f16d0261a4695c96d4af6
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-06 13:28:48 +0100

    new service added

--- demo_service/app.py
+++ demo_service/app.py
@@ -1,1 +1,9 @@
-print('Hello DX')
+import sys
+import json
+from ferris_cli import ApplicationConfigurator
+from ferris_cli import EventSender
+
+event_type = "my_custom_event_type"
+data = {"some_parameter" : "Hello from Hello World"}
+
+EventSender().send(event_type.data)
---
+++ hello_world_2/app_1.py
@@ -0,0 +1,5 @@
+import sys
+import json
+
+payload = json.loads(sys.args[1])
+print(payload)
