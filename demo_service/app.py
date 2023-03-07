b1151ddd0df1c60cc72e15a2cfc2152319656126
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-07 10:02:56 +0100

    ferris changes

--- demo_service/app.py
+++ demo_service/app.py
@@ -1,9 +1,10 @@
 import sys
 import json
 
-from ferris_cli import EventSender
+from ferris_cli.ferri_cli import ApplicationConfigurator
+from ferris_cli.v2 import FerrisEvents
 
 event_type = "my_custom_event_type"
 data = {"some_parameter" : "Hello from Hello World"}
 
-EventSender().send(event_type.data)
+FerrisEvents().send(event_type.data)
