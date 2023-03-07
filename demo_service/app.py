6aadc9764bc0defcca136d7ae00826c2b608b8cd
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-07 10:07:18 +0100

    ferris changes#

--- demo_service/app.py
+++ demo_service/app.py
@@ -1,7 +1,7 @@
 import sys
 import json
 
-from ferris_cli.ferri_cli import ApplicationConfigurator
+from ferris_cli.ferris_cli import ApplicationConfigurator
 from ferris_cli.v2 import FerrisEvents
 
 event_type = "my_custom_event_type"
