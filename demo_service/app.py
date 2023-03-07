9fbc659dba8f8821fd9f090663fa9365b75b70a1
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-07 10:50:11 +0100

    update app.py

--- demo_service/app.py
+++ demo_service/app.py
@@ -3,8 +3,10 @@ import json
 
 from ferris_cli.ferris_cli import ApplicationConfigurator
 from ferris_cli.v2 import FerrisEvents
+from ferris_ef import get_param
 
 event_type = "my_custom_event_type"
 data = {"some_parameter" : "Hello from Hello World"}
+event_source = get_param('package_name')
 
-FerrisEvents().send(event_type=event_type, data=data)
+FerrisEvents().send(event_type=event_type, event_source=event_source, data=data)
