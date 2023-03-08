535cf7347b3557126a1082955a07648d40ed3a6f
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-08 13:45:44 +0100

    hello_context update

--- hello_context/app.py
+++ hello_context/app.py
@@ -8,8 +8,6 @@ print("I am step 2")
 
 print('hello dx')
 
-
-
 print("------- Get Service Config -------")
 print(context.config.get("my_config"))
 
@@ -26,4 +24,18 @@ print(context.state.get())
 print("------- Update Service State -------")
 context.state.put("some_key", "some_value")
 context.state.put("other_key", {"attr_1": "val_1"})
+
+print("------- Get Service State -------")
+print(context.state.get())
+
+
+print("------- Get Secret -------")
+print(context.secrets.get("test_secret_1"))
+
+print("------- Set Secret -------")
+print(context.secrets.set("test_secret_4", {"pfkey":"platform val"}, "platform"))
+print(context.secrets.set("test_secret_4", {"prjkey":"project val"}, "project"))
+
+print("------- Get Secret test_secret_4 -------")
+print(context.secrets.get("test_secret_4"))
 
