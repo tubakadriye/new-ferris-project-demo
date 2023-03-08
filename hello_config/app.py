70ad338c44e0f0ea2bc2d743718465cbc7015ef5
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-08 14:10:45 +0100

    hello_config my_config update

--- hello_config/app.py
+++ hello_config/app.py
@@ -11,9 +11,6 @@ print('hello dx')
 platform_config = ApplicationConfigurator().get("ferris.env")
 print(platform_config)
 
-platform_config_demo = ApplicationConfigurator().get("demoapp.env")
-print(platform_config_demo)
-
-my_config = ApplicationConfigurator().get("ferris.services.hello_config.config")["my_config"]
+my_config = ApplicationConfigurator().get("ferris.packages.tuba.hello_config.config")["my_config"]
 print(my_config)
 
