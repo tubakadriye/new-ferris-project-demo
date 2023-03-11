5e3558796b8041deec17b32f6da2b9d0302f5d62
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-11 12:24:32 +0100

    hello_state update

--- hello_state/app.py
+++ hello_state/app.py
@@ -1,5 +1,5 @@
 from ferris_ef import context
 
-my_state = context.state.get() # returns a state previously set
+my_last_state = context.state.get() # returns a state previously set
 some_value = my_last_state.get('key')
 context.state.put('Key','Value')
