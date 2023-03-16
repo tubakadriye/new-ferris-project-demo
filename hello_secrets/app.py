8455065fa5043ed8c763ff527920300a6c807636
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-03-16 10:53:50 +0100

    tweet sentiment

--- hello_secrets/app.py
+++ hello_secrets/app.py
@@ -1,4 +1,6 @@
-from ferris_ef import get_secret
+from ferris_ef import get_secret, get_param
 
 print(f"DB NAME: {get_secret('DB_NAME')}")
 print(f"DB PASS: {get_secret('DB_PASS')}")
+
+print(f"service Name: {get_param('service_name')}")
---
+++ tweet_sentiment/app.py
@@ -0,0 +1,27 @@
+"""
+Perform tweet sentiment analysis using OpenAI API.
+Sentiment classifications are: positive, neutral, or negative
+"""
+
+import json
+import sys
+import openapi
+from ferris_ef import context
+
+payload = json.loads(sys.argv[1])
+tweet = payload("tweet_text")
+print("Tweet to analyze:" {tweet})
+
+openai.api_key = context.secrets.get("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+    model = "text-davinci-003",
+    prompt = f'Decide whether a Tweet\'s sentiment is positive, neutral, or negative.\n\nTweet: "{tweet}"\nSentiment:',
+    temperature=0.7,
+    max_tokens=60,
+    top_p=1.0,
+    frequency_penalty=0.5,
+    presence_penalty=0.0,
+)
+
+print(f"Tweet sentiment: {response}")
