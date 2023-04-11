970c09e6fa6ede764691e36730975b47a11dc376
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-04-11 13:38:57 +0200

    update get set secrets

--- hello_get_set_secrets/app.py
+++ hello_get_set_secrets/app.py
@@ -6,10 +6,13 @@ print("------- Get Secret -------")
 print(context.secrets.get("test_secret_1"))
 
 print("------- Set Secret -------")
-print(context.secrets.set(name = "test_secret_4", value = {"pfkey":"platform val"}, context = "platform"))
-print(context.secrets.set("test_secret_4", {"prjkey":"project val"}, "project"))
+print(context.secrets.set(name = "test_secret_4", value = {"test_secret_4":"55"}, context = "platform"))
+print(context.secrets.set("test_secret_5", {"test_secret_5":"17"}, "project"))
 
 print("------- Get Secret test_secret_4 -------")
 print(context.secrets.get("test_secret_4"))
 
+
+print("------- Get Secret test_secret_5 -------")
+print(context.secrets.get("test_secret_5"))
 
