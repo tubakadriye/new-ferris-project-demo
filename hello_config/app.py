41b907ced40b0348cdc27c41d41ec0599c48a044
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-04-19 11:38:16 +0200

    change in chart

--- hello_config/app.py
+++ hello_config/app.py
@@ -12,3 +12,13 @@ print(platform_config)
 my_config = context.config.get("my_config")
 print("my_config:" , my_config)
 
+
+charts = ApplicationConfigurator.get().get("PROJECT_CHARTS", [])
+
+formatted_urls = []
+
+for ch in charts:
+    ch['ENDPOINT'] = f"{ch['ENDPOINT']}&project_id={pk}"
+    print(ch['ENDPOINT'], flush=True)
+    formatted_urls.append(ch)
+
